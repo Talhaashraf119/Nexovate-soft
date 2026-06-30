65de3c529f8664a14cad9be32983569bcf001649
Talha <talhaashraf119@gmail.com>
2026-06-30 11:28:21 +0500

    Fix CORS configuration

--- server.js
+++ server.js
@@ -3,18 +3,27 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
 import developerRoutes from './routes/developerRoutes.js';
+import cors from 'cors'
 
 dotenv.config({ quiet:true});
 
+
 const app = express();
-
-
+app.use(cors({
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 app.use(express.json());
 
 
 app.use('/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
-app.use('/api/developers', projectRoutes);
+app.use('/api/developers', developerRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
